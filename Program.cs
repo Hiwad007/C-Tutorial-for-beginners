29cfd542b380468bbc0bfb169cea9398ca542905
Hiwad007 <122149265+Hiwad007@users.noreply.github.com>
2023-09-23 00:42:04 +0500

    Add files via upload
    calculator in C#

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int num01;
+            int num02;
+            Console.WriteLine("type a number to be multiplied:");
+            num01 = Convert.ToInt32 (Console.ReadLine());
+            Console.Write("type another number:");
+            num02 = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("the result is "+ num01 * num02);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
